c4004a800c3f483fc012fb3b3978be4cd3374745
Isabela Cardona <1111481156@u.icesi.edu.co>
2025-09-23 13:04:43 -0500

    ultimos cambios

--- vite-project/src/pages/PropsPage.tsx
+++ vite-project/src/pages/PropsPage.tsx
@@ -1,39 +1,47 @@
-import {useState} from "react";
+import {useEffect, useState} from "react";
 import UserCard from "../components/UserCard";
 import type { User } from "../types/UserType";
 
 function PropsPage() {
     const [showDetails, setShowDetailes] = useState(true);  //userstate
 
-    const user: User = {
-        id: 1,
-        username: "Kelo",
-        email: "kevin@gmail.com"
-    };
+    //-------------- JSON -----------------------
+    const [users, setUsers] = useState<User[]>([]);
+    const [recap, setRecap] = useState(0);
 
-    const user2: User = {
-        id: 2,
-        username: "Isabela",
-        email: "isabela@gmail.com"
-    };
+    useEffect (() => {
+        async function getUsers () {
+            const data = await fetch ("/data/users.json");
+            const users = await data.json();
+            setUsers(users);
+        }
+
+        getUsers();
+    },[recap]); 
+    //-------------------------------------
 
     function handleClick () { //userstate cuando los oculte o los muestre
         console.log ("Me estan dando click");
         setShowDetailes (prev => !prev);
     }
 
+    function getInfo () {
+        setRecap(prev => prev+1);
+    }
+
     return (
         <div id="props-page-container">
             <h1>Página de Props</h1>
+            <button onClick={getInfo}>Solicitar info</button>
             <button onClick={handleClick}>presioname</button>
-            <UserCard 
-                userToShow={user} 
-                hide={showDetails}
-            />
-            <UserCard 
-                userToShow={user2} 
-                hide={showDetails}
-            />
+            {
+                users.map(user => (
+                    <UserCard 
+                        userToShow={user}
+                        hide={showDetails}
+                    />
+                ))
+            }
         </div>
     );
 }
