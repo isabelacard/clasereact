509b2b8393ae002df9753cfe15a60b2cde243e50
Isabela Cardona <1111481156@u.icesi.edu.co>
2025-09-23 12:34:40 -0500

    Se hicieron cambios de estilos y mostrar

--- vite-project/src/components/UserCard.tsx
+++ vite-project/src/components/UserCard.tsx
@@ -1,11 +1,11 @@
 import type { UserCardProps } from "../types/UserType";
 
 function UserCard({userToShow,hide}: UserCardProps) {
+
     return (
-        <div id="user-card">
-            <h2>{userToShow.username}</h2>
-            <h2>{userToShow.email}</h2>
-            <p>{hide}</p>
+        <div id="user-card" className="bg-pink-400 p-3 rounded-3xl m-3 shadow-2xl hover:bg-amber-400 hover:scale-110 transition-all">
+            <h2 className="font-bold text-2xl">{userToShow.username}</h2>
+            {hide && <h2>{userToShow.email}</h2>}
         </div>
     );
 }
--- vite-project/src/pages/PropsPage.tsx
+++ vite-project/src/pages/PropsPage.tsx
@@ -1,19 +1,38 @@
+import {useState} from "react";
 import UserCard from "../components/UserCard";
 import type { User } from "../types/UserType";
 
 function PropsPage() {
+    const [showDetails, setShowDetailes] = useState(true);  //userstate
+
     const user: User = {
         id: 1,
         username: "Kelo",
         email: "kevin@gmail.com"
     };
 
+    const user2: User = {
+        id: 2,
+        username: "Isabela",
+        email: "isabela@gmail.com"
+    };
+
+    function handleClick () { //userstate cuando los oculte o los muestre
+        console.log ("Me estan dando click");
+        setShowDetailes (prev => !prev);
+    }
+
     return (
         <div id="props-page-container">
             <h1>Página de Props</h1>
+            <button onClick={handleClick}>presioname</button>
             <UserCard 
                 userToShow={user} 
-                hide={false}
+                hide={showDetails}
+            />
+            <UserCard 
+                userToShow={user2} 
+                hide={showDetails}
             />
         </div>
     );
